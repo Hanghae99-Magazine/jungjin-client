49be71a18187c8d9120cc6c2c7458a4034c5c4d8
kimjungjin <jungks9351@gmail.com>
2022-02-21 20:39:37 +0900

    Feat 회원가입 이메일 형식 확인 기능 추가

--- src/pages/RegistPage.jsx
+++ src/pages/RegistPage.jsx
@@ -1,20 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 import CommonTemplate from '../components/common/CommonTemplate';
+import useInputValue from '../hooks/useInputValue';
 
 const RegistPage = () => {
+  const userId = useInputValue('');
+  const nickname = useInputValue('');
+  const userPw = useInputValue('');
+  const userPwConfirm = useInputValue('');
+
+  // 임시 토큰
+  const token = 1;
+
+  // 이메일 형식 체크
+  const checkEmailId = (userId) => {
+    const regexr =
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    return regexr.test(userId);
+  };
+
+  const registUser = async (e) => {
+    e.preventDefault();
+
+    if (!checkEmailId(userId)) {
+      alert('아이디는 이메일 형식으로 작성해주세요');
+      return;
+    }
+
+    const payload = {
+      userId: userId.value,
+      nickname: nickname.value,
+      userPw: userPw.value,
+      token,
+    };
+
+    await axios.post('http://localhost:4000/users', payload);
+  };
   return (
     <CommonTemplate>
       <h2>회원가입</h2>
-      <RegistForm>
-        <input id="userId" type="id" placeholder="아이디" />
-        <input id="password" type="password" placeholder="비밀번호" />
+      <RegistForm onSubmit={registUser}>
+        <input id="userId" type="id" placeholder="아이디" autoComplete="off" />
+        <input
+          id="password"
+          type="password"
+          placeholder="비밀번호"
+          autoComplete="off"
+          onChange={userPw.onChange}
+        />
         <input
           id="passwordConfirm"
           type="password"
           placeholder="비밀번호 확인"
+          autoComplete="off"
+          onChange={userPwConfirm.onChange}
         />
-        <input id="nickName" type="text" placeholder="닉네임" />
+        <input
+          id="nickName"
+          type="text"
+          placeholder="닉네임"
+          autoComplete="off"
+          onChange={nickname.onChange}
+        />
         <button id="registBtn" className="regist-btn">
           회원가입
         </button>
@@ -36,7 +84,7 @@ const RegistForm = styled.form`
     display: block;
     border: 1px solid #000;
     margin: 20px 30px 0px;
-    color: #fff;
+    color: #000;
     padding: 15px 10px;
     border-radius: 5px;
     font-size: 1.6rem;

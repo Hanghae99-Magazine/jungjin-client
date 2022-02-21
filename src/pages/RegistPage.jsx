d369f58eba65a9df05b7a35155e5082cf0c76483
kimjungjin <jungks9351@gmail.com>
2022-02-21 15:21:01 +0900

    Style RegistPage 구현

--- src/pages/RegistPage.jsx
+++ src/pages/RegistPage.jsx
@@ -1,32 +1,61 @@
 import React from 'react';
 import styled from 'styled-components';
+import CommonTemplate from '../components/common/CommonTemplate';
 
 const RegistPage = () => {
   return (
-    <RegistWrapper>
-      <label for="userId">
-        아이디 : <input id="userId" type="text" />
-      </label>
-      <label for="nickname">
-        닉네임 : <input id="nickname" type="text" />
-      </label>
-      <label for="userPw">
-        비밀번호: <input id="userPw" type="text" />
-      </label>
-      <label for="userPwConfirm">
-        비밀번호 확인: <input id="userPwConfirm" type="text" />
-      </label>
-      <button id="addUserBtn">회원가입</button>
-    </RegistWrapper>
+    <CommonTemplate>
+      <h2>회원가입</h2>
+      <RegistForm>
+        <input id="userId" type="id" placeholder="아이디" />
+        <input id="password" type="password" placeholder="비밀번호" />
+        <input
+          id="passwordConfirm"
+          type="password"
+          placeholder="비밀번호 확인"
+        />
+        <input id="nickName" type="text" placeholder="닉네임" />
+        <button id="registBtn" className="regist-btn">
+          회원가입
+        </button>
+      </RegistForm>
+    </CommonTemplate>
   );
 };
 
-const RegistWrapper = styled.form`
+const RegistForm = styled.form`
   display: flex;
+  justify-content: center;
+  align-items: center;
   flex-direction: column;
   font-size: 1.4rem;
   gap: 2rem;
   margin: 3rem;
+  input {
+    width: 300px;
+    display: block;
+    border: 1px solid #000;
+    margin: 20px 30px 0px;
+    color: #fff;
+    padding: 15px 10px;
+    border-radius: 5px;
+    font-size: 1.6rem;
+  }
+  input::placeholder {
+    font-size: 1.6rem;
+    color: #8c8c8c;
+  }
+  .regist-btn {
+    width: 300px;
+    margin: 20px 30px;
+    padding: 15px 10px;
+    text-align: center;
+    background-color: #000;
+    color: #fff;
+    border: 1px solid #000;
+    border-radius: 5px;
+    font-size: 1.6rem;
+  }
 `;
 
 export default RegistPage;

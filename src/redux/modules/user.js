97ac7aa18d904765021f802f2f5eade06323415e
kimjungjin <jungks9351@gmail.com>
2022-02-22 18:17:52 +0900

    Feat login redux-toolkit 구현

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,51 +1,33 @@
-import { createAction, handleActions } from 'redux-actions';
-import produce from 'immer';
-import { deleteCookie, setCookie } from '../../shared/Cookie';
+import { createSlice } from '@reduxjs/toolkit';
 
-// 초기값 설정
 const initialState = {
   user: null,
   isLogin: false,
   err: null,
 };
 
-// 액션 타입 정의
+// toolkit slice
 
-const LOGIN = 'auth/LOGIN';
-const LOGOUT = 'auth/LOGOUT';
-const GET_USER = 'auth/GET_USER';
-
-// 액션 생성 함수
-
-export const logIn = createAction(LOGIN, (user) => ({ user }));
-export const logOut = createAction(LOGOUT, (user) => ({ user }));
-export const getUser = createAction(GET_USER, (user) => ({ user }));
-
-const User = handleActions(
-  {
-    [LOGIN]: (state, action) =>
-      produce(state, (draft) => {
-        setCookie('myToken', `success`, 3);
-        draft.user = action.payload.user;
-        draft.isLogin = true;
-      }),
-    [LOGOUT]: (state, action) =>
-      produce(state, (draft) => {
-        deleteCookie('myToken');
-        draft.user = null;
-        draft.is_login = false;
-      }),
-    [GET_USER]: (state, action) => produce(state, (draft) => {}),
-  },
+const userSlice = createSlice({
+  name: 'user',
   initialState,
-);
+  reducers: {
+    login(state) {
+      state.isLogin = true;
+      state.err = null;
+    },
+    loginSuccess(state, action) {
+      state.isLogin = true;
+      state.err = null;
+      state.user = action.payload;
+    },
+    loginFailure(state, action) {
+      state.isLogin = false;
+      state.err = action.payload;
+    },
+  },
+});
 
-const actionCreators = {
-  logIn,
-  logOut,
-  getUser,
-};
+export const { login, loginSuccess, loginFailure } = userSlice.actions;
 
-export { actionCreators };
-
-export default User;
+export default userSlice.reducer;

dc26ab4f3d0f0e23b600596399743a4d0b3f1371
kimjungjin <jungks9351@gmail.com>
2022-02-22 18:16:23 +0900

    Feat reduxtoolkit, redux-saga react 연결

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,37 +1,32 @@
-import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import { createBrowserHistory } from 'history';
-import { connectRouter } from 'connected-react-router';
+import { combineReducers } from 'redux';
+import { all } from 'redux-saga/effects';
+import createSagaMiddleware from 'redux-saga';
+import user from './modules/user';
+import { userSaga } from '../saga/userSaga';
+import { configureStore } from '@reduxjs/toolkit';
 
-import User from './modules/user';
+const reducer = combineReducers({ user });
 
-export const history = createBrowserHistory();
+export const sagaMiddleware = createSagaMiddleware();
 
-const rootReducer = combineReducers({
-  user: User,
-  router: connectRouter(history),
-});
+const middleware = [sagaMiddleware];
 
-const middlewares = [thunk];
+// 지금이 어느 환경인 지 알려줌 (개발환경, 프로덕션(배포)환경 ...)
+const devMode = process.env.NODE_ENV === 'development';
 
-// 지금이 어느 환경인 지 알려줘요. (개발환경, 프로덕션(배포)환경 ...)
-const env = process.env.NODE_ENV;
-
-// 개발환경에서는 로거라는 걸 하나만 더 써볼게요.
-if (env === 'development') {
+// 개발자 환경에서만 로거 미들웨어 추가
+if (devMode) {
   const { logger } = require('redux-logger');
-  middlewares.push(logger);
+  middleware.push(logger);
 }
 
-const composeEnhancers =
-  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
+export function* rootSaga() {
+  yield all([userSaga()]);
+}
 
-const enhancer = composeEnhancers(applyMiddleware(...middlewares));
-
-let store = (initialStore) => createStore(rootReducer, enhancer);
-
-export default store();
+export const store = configureStore({
+  reducer,
+  devTools: devMode,
+  middleware,
+});
+sagaMiddleware.run(rootSaga);

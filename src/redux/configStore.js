e6c491ffdeb1acf83acf5d70e738d80e6f31c657
kimjungjin <jungks9351@gmail.com>
2022-02-23 03:03:37 +0900

    Feat posts reducer, saga 추가

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,14 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import createSagaMiddleware from 'redux-saga';
-import user from './modules/user';
-import { userSaga } from '../saga/userSaga';
 import { configureStore } from '@reduxjs/toolkit';
 
-const reducer = combineReducers({ user });
+import user from './modules/user';
+import { userSaga } from '../saga/userSaga';
+import posts from './modules/posts';
+import { postsSaga } from '../saga/postsSaga';
+
+const reducer = combineReducers({ user, posts });
 
 export const sagaMiddleware = createSagaMiddleware();
 
@@ -14,14 +17,14 @@ const middleware = [sagaMiddleware];
 // 지금이 어느 환경인 지 알려줌 (개발환경, 프로덕션(배포)환경 ...)
 const devMode = process.env.NODE_ENV === 'development';
 
-// 개발자 환경에서만 로거 미들웨어 추가
-if (devMode) {
-  const { logger } = require('redux-logger');
-  middleware.push(logger);
-}
+// // 개발자 환경에서만 로거 미들웨어 추가
+// if (devMode) {
+//   const { logger } = require('redux-logger');
+//   middleware.push(logger);
+// }
 
 export function* rootSaga() {
-  yield all([userSaga()]);
+  yield all([userSaga(), postsSaga()]);
 }
 
 export const store = configureStore({

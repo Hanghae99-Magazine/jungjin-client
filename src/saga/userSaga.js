5d52bfbf16e866eaef11ebcb25e6ae64036c9cd4
kimjungjin <jungks9351@gmail.com>
2022-02-23 18:46:07 +0900

    Feat 로그아웃 saga 추가

--- src/saga/userSaga.js
+++ src/saga/userSaga.js
@@ -1,8 +1,8 @@
 import * as userAPI from '../api/user';
 
 import { put, call, takeLatest } from 'redux-saga/effects';
-import { login } from '../redux/modules/user';
-import { setCookie } from '../shared/Cookie';
+import { login, logOut } from '../redux/modules/user';
+import { deleteCookie, setCookie } from '../shared/Cookie';
 
 //saga
 
@@ -11,7 +11,17 @@ function* loginSaga(action) {
     const user = yield call(() => userAPI.login(action.payload));
     setCookie('myToken', 'sucess', 3);
     console.log(user);
+    console.log(action.payload);
     yield put({ type: `${action.type}Success`, payload: action.payload });
+  } catch (err) {
+    yield put({ type: `${action.type}Failure`, payload: action.payload });
+  }
+}
+
+function* logOutSaga(action) {
+  try {
+    deleteCookie('myToken');
+    yield put({ type: `${action.type}Success` });
   } catch (err) {
     yield put({ type: `${action.type}Failure`, payload: action.payload });
   }
@@ -21,4 +31,5 @@ function* loginSaga(action) {
 
 export function* userSaga() {
   yield takeLatest(login, loginSaga);
+  yield takeLatest(logOut, logOutSaga);
 }

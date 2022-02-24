edc297fa9a32e89d78b1ed584beb48fde2850015
kimjungjin <jungks9351@gmail.com>
2022-02-24 19:41:49 +0900

    Feat 로그인 했을 경우 로그인페이지 이동시 메인페이지 이동 구현

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CommonTemplate from '../components/common/CommonTemplate';
 import { getCookie } from '../shared/Cookie';
 import CommonHeader from '../components/common/CommonHeader';
 import LoginForm from '../components/login/LoginForm';
+import { useSelector } from 'react-redux';
 
 const LoginPage = () => {
-  const [token, setToken] = useState(false);
   const navigate = useNavigate();
 
+  const isLogin = useSelector(({ user }) => {
+    return user.isLogin;
+  });
+
   useEffect(() => {
-    let myToken = getCookie('mytoken');
-    myToken ? setToken(true) : setToken(false);
-    if (token) {
+    if (isLogin) {
+      alert('이미 로그인 하셨습니다.');
       navigate('/');
     }
-  }, [navigate, token]);
+  }, [isLogin]);
   return (
     <>
       <CommonHeader />

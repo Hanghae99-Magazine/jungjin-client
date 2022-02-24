95bb912c5035e692fff2a710e02279e20499a912
kimjungjin <jungks9351@gmail.com>
2022-02-24 23:18:02 +0900

    Feat 포스트 추가 비동기 통신

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,15 @@
 import client from './client';
 
 export const getPosts = async () => {
-  const res = await client.get('/posts');
+  const res = await client.get('/post');
+  return res;
+};
+
+export const addPost = async ({ img_position, post_img, post_content }) => {
+  const res = await client.post('/post', {
+    img_position,
+    post_img,
+    post_content,
+  });
   return res;
 };

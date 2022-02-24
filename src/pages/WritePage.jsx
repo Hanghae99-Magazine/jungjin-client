09c43ce78379176bb693d49b180c5d3763c32dbe
kimjungjin <jungks9351@gmail.com>
2022-02-24 23:21:27 +0900

    Remove 불필요 코드 제거

--- src/pages/WritePage.jsx
+++ src/pages/WritePage.jsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import CommonHeader from '../components/common/CommonHeader';
 import CommonTemplate from '../components/common/CommonTemplate';
-import WriteForm from '../components/writeLayout/WriteForm';
+import WriteForm from '../components/write/WriteForm';
+import { getCookie } from '../shared/Cookie';
 
 const WritePage = () => {
   return (

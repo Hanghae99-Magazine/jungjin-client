0703ad7911007a37e3769e1a01f4d2aed1fb92e7
kimjungjin <jungks9351@gmail.com>
2022-02-28 04:39:49 +0900

    Feat 게시물 삭제 구현

--- src/api/posts.js
+++ src/api/posts.js
@@ -13,3 +13,13 @@ export const addPost = async ({ img_position, post_img, post_content }) => {
   });
   return res;
 };
+
+export const deletePost = async (post_id) => {
+  try {
+    const res = await client.delete(`/post/${post_id}`, { post_id });
+    alert('삭제완료!');
+    return res;
+  } catch (err) {
+    alert(err.response.data.msg);
+  }
+};
--- src/components/main/PostItme.jsx
+++ src/components/main/PostItme.jsx
@@ -1,25 +1,56 @@
 import React from 'react';
 import styled from 'styled-components';
+import { AiFillDelete } from 'react-icons/ai';
+import { IoMdCreate } from 'react-icons/io';
+import { deletePost } from '../../api/posts';
+import { useNavigate } from 'react-router-dom';
 
 const PostItme = ({ postData }) => {
+  console.log(postData);
+  const navigate = useNavigate();
+
+  const nickname = sessionStorage.getItem('nickname');
+
+  const handleUpdateBtn = () => {
+    navigate(`/write/update/${postData.post_id}`);
+  };
+
+  const handleDeleteBtn = async () => {
+    await deletePost(postData.post_id);
+    navigate('/');
+  };
   return (
     <PostItemWrapper>
-      <div className="post-img-container">
-        <img src={postData.post_img} alt="postImg" className="post-img" />
-      </div>
-      <div className="post-info">
-        <p className="post-content">{postData.post_content}</p>
-      </div>
       <div className="user-info-container">
         <div className="user-profile-info">
           <span>{postData.nickname}</span>
         </div>
-        <div className="like">
-          <div className="like-icon">좋아요</div>
+        <div className="sub-info">
+          <span>{postData.upload_date}</span>
         </div>
+        {nickname === postData.nickname ? (
+          <div className="update-container">
+            <IoMdCreate className="update-btn" />
+            <AiFillDelete className="delete-btn" onClick={handleDeleteBtn} />
+          </div>
+        ) : (
+          ''
+        )}
       </div>
-      <div className="sub-info">
-        <span>{postData.upload_date}</span>
+      <div className="post-info">
+        <div className="post-img-container">
+          <img src={postData.post_img} alt="postImg" className="post-img" />
+        </div>
+        <TestWrapper
+          className="post-content"
+          post_img={`url(${postData.post_img})`}
+          // style={{ background: `url(${postData.post_img})` }}
+        >
+          {postData.post_content}
+        </TestWrapper>
+      </div>
+      <div className="like">
+        <div className="like-icon">좋아요</div>
         <span>좋아요 수 {postData.post_like}</span>
       </div>
     </PostItemWrapper>
@@ -30,12 +61,42 @@ const PostItemWrapper = styled.li`
   width: 70%;
   padding: 0 1rem;
   margin: 2rem auto;
-  box-shadow: 0px 5px 5px 0px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px -1px 5px 5px rgba(0, 0, 0, 0.2);
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
   overflow: hidden;
-  font-size: 1.4rem;
+  .user-info-container {
+    padding: 0.5rem;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-size: 1.6rem;
+    font-weight: bolder;
+    .delete-btn:hover {
+      cursor: pointer;
+      color: red;
+    }
+  }
+  .update-container {
+    font-size: 3rem;
+    display: flex;
+    gap: 1rem;
+    .update-btn:hover {
+      cursor: pointer;
+      color: #eeeee2;
+    }
+    .delete-btn:hover {
+      cursor: pointer;
+      color: #ff0000;
+    }
+  }
+  .like {
+    margin-left: 0.5rem;
+    display: flex;
+    align-items: center;
+    gap: 0.2rem;
+  }
   .post-img {
     width: 100%;
     object-fit: cover;
@@ -53,34 +114,26 @@ const PostItemWrapper = styled.li`
       height: 3rem;
     }
   }
-  .user-info-container {
-    padding: 0 0.5rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    .user-profile-info {
-      display: flex;
-      align-items: center;
-      .user-profile-img {
-        width: 20px;
-        height: 20px;
-        border-radius: 100%;
-        margin-right: 0.5rem;
-      }
-    }
-    .like {
-      margin-left: 0.5rem;
-      display: flex;
-      align-items: center;
-      gap: 0.2rem;
-    }
-  }
+
+  /* .post-content {
+    white-space: pre-line;
+    width: 300px;
+    height: 200px;
+  } */
+
   .sub-info {
     padding: 0.5rem;
     font-size: 1.2rem;
     display: flex;
     justify-content: space-between;
   }
+`;
+
+const TestWrapper = styled.div`
+  width: 100%;
+  background-image: ${(props) => props.post_img};
+  background-size: cover;
+  display: inline-block;
 `;
 
 export default PostItme;

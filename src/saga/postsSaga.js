a4ab3b9efd6b3895b38fc8a1c8758e2985a4b7f7
kimjungjin <jungks9351@gmail.com>
2022-03-02 00:28:39 +0900

    Refactor S3 이미지 API 분리

--- src/saga/postsSaga.js
+++ src/saga/postsSaga.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '../api/posts';
+import * as s3ImgAPI from '../api/s3Image';
 
 import { put, call, takeLatest } from 'redux-saga/effects';
 import {
@@ -10,7 +11,6 @@ import {
 } from '../redux/modules/posts';
 
 import { v4 as uuidv4 } from 'uuid';
-import * as s3ImgAPI from '../api/image';
 
 function* getPostsSaga(action) {
   try {
